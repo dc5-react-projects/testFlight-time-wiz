9a9f85915ba58a2e65b82cd842de8f4a358e8c40
d-chauhan5 <d.chauhan5@outlook.com>
2026-03-28 15:31:28 +0530

    React Time Wiz - TestFlight -  React Project

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,6 @@ function App() {
   return (
     <>
       <Index />
-      {/* <section id="center">
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section> */}
     </>
   );
 }
